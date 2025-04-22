d642193ff4b048d75192bfcdf31c15e89c926020
Tilley <tilleyy8@gmail.com>
2025-04-22 15:54:02 -0600

    remove stuff

--- TilleyBot.py
+++ TilleyBot.py
@@ -1,7 +1,6 @@
-import discord, asyncio, random, os, re, requests, base64, subprocess, http, json, csv, time
+import discord, random, os, re, requests, subprocess, http, json, csv, time
 from datetime import datetime, timedelta
 from discord.ext import commands
-from hashlib import sha256
 from html import unescape
 from openai import OpenAI
 
@@ -12,21 +11,14 @@ except ModuleNotFoundError:
     from Crypto.Util.Padding import pad, unpad
     from Crypto.Cipher import AES
 
-user_gmt_offset = -7
 bot_token_file = "~/bot_tokens/TilleyBot.token"
 user_token_file = "~/bot_tokens/tillay8.token"
 ai_token_file = "~/bot_tokens/deepseek.token"
-
-key_file = "/tmp/key"
 
 bot = commands.Bot("!", intents=discord.Intents.none())
 
 def get_bot_token():
     with open(os.path.expanduser(bot_token_file), 'r') as f:
-        return f.readline().strip()
-
-def get_passwd():
-    with open(os.path.expanduser(key_file), 'r') as f:
         return f.readline().strip()
 
 def get_user_token():
@@ -89,43 +81,6 @@ def get_most_recent_message(channel_id):
     else:
         return None
 
-def send_file(channel_id, file_path):
-    conn = http.client.HTTPSConnection("discord.com", 443)
-    with open(file_path, 'rb') as file:
-        file_data = file.read()
-    boundary = '----WebKitFormBoundary' + ''.join(random.choices('abcdefghijklmnopqrstuvwxyz0123456789', k=16))
-    body = (
-        f'--{boundary}\r\n'
-        f'Content-Disposition: form-data; name="file"; filename="{os.path.basename(file_path)}"\r\n'
-        'Content-Type: application/octet-stream\r\n\r\n'
-    ).encode('utf-8') + file_data + f'\r\n--{boundary}--\r\n'.encode('utf-8')
-    headers = {
-        "Content-Type": f"multipart/form-data; boundary={boundary}",
-        "Authorization": header_data["Authorization"]
-    }
-    conn.request("POST", f"/api/v10/channels/{channel_id}/messages", body, headers)
-
-def channel_name_from_id(channel_id):
-    conn = http.client.HTTPSConnection("discord.com", 443)
-    conn.request("GET", f"/api/v10/channels/{channel_id}", headers=header_data)
-    response = conn.getresponse()
-    data = response.read().decode('utf-8')
-    if response.status == 200:
-        channel_data = json.loads(data)
-        guild_id = channel_data.get("guild_id")
-        channel_name = channel_data.get("name")
-        conn.request("GET", f"/api/v10/guilds/{guild_id}", headers=header_data)
-        guild_response = conn.getresponse()
-        guild_data = guild_response.read().decode('utf-8')
-        if guild_response.status == 200:
-            guild_info = json.loads(guild_data)
-            guild_name = guild_info.get("name")
-            return [guild_name, channel_name]
-        else:
-            return None
-    else:
-        return None
-
 def get_catgirl_link():
     url = "https://nekos.moe/api/v1/random/image"
     response = requests.get(url, params={'nsfw': False})
@@ -139,7 +94,7 @@ def translator(inp, to):
         url = f"https://translate.google.com/m?sl=auto&tl={to}&hl=en&q={encoded_input}"
         response = requests.get(url)
         match = re.search(r'class="result-container">([^<]*)</div>', response.text)
-        
+
         if match:
             translated_text = match.group(1)
             return unescape(translated_text)
@@ -147,16 +102,6 @@ def translator(inp, to):
             return "Translation failed"
     except Exception as e:
         return "Translation failed"
-
-def tcrypt(plaintext, passphrase):
-    key, iv = sha256((passphrase).encode()).digest(), os.urandom(AES.block_size)
-    return base64.b64encode(iv + AES.new(key, AES.MODE_CBC, iv).encrypt(pad(plaintext.encode(), AES.block_size))).decode()
-
-def tdcrypt(ciphertext, passphrase):
-    try:
-        decoded = base64.b64decode(ciphertext)
-        return unpad(AES.new(sha256((passphrase).encode()).digest(), AES.MODE_CBC, decoded[:AES.block_size]).decrypt(decoded[AES.block_size:]), AES.block_size).decode()
-    except (ValueError, KeyError): return None
 
 def execute_command(cmd):
     try:
@@ -169,23 +114,11 @@ def execute_command(cmd):
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
-@bot.tree.command(name="echo", description="Send a message as tillay8")
-async def echo(interaction: discord.Interaction, tosay: str, channel_id: str = None):
-    channel_id = interaction.channel.id if not channel_id else int(channel_id)
-    await interaction.response.send_message("Echoing", ephemeral=True)
-    send_user_message(channel_id, tosay)
-
-@bot.tree.command(name="test", description="Is bot alive?")
-async def test(interaction: discord.Interaction):
-    await interaction.response.send_message(interaction.channel, ephemeral=True)
-
-@bot.tree.command(name="maze", description="Generate maze")
-async def maze(interaction: discord.Interaction, size: int, channel_id: str = None):
-    channel_id = interaction.channel.id if not channel_id else int(channel_id)
-    await interaction.response.send_message("Generating maze", ephemeral=True)
-    send_user_message(channel_id, f"-maze {size}")
-    await asyncio.to_thread(os.system, f"./maze {size} maze.png")
-    send_file(channel_id, "./maze.png")
+@bot.tree.command(name="repeat", description="repeat previous message")
+async def repeat(interaction: discord.Interaction):
+    channel_id = interaction.channel.id
+    await interaction.response.send_message("repeating", ephemeral=True)
+    send_user_message(channel_id, get_most_recent_message(interaction.channel.id))
 
 @bot.tree.command(name="pfp", description="Send pfp of a user")
 async def pfp(interaction: discord.Interaction, user: str):
@@ -200,14 +133,6 @@ async def pfp(interaction: discord.Interaction, user: str):
 async def printas(interaction: discord.Interaction, message: str):
     await interaction.response.send_message(message)
 
-@bot.tree.command(name="encrypt", description="encrypt a message using server password")
-async def encrypt(interaction: discord.Interaction, message: str):
-    await interaction.response.send_message(f"&&{tcrypt(message, get_passwd())}", ephemeral=True)
-
-@bot.tree.command(name="decrypt", description="decrypt a message using server password")
-async def decrypt(interaction: discord.Interaction, encrypted: str):
-    await interaction.response.send_message(tdcrypt(encrypted[2:], get_passwd()), ephemeral=True)
-
 @bot.tree.command(name="scramble", description="scramble text")
 async def scramble(interaction: discord.Interaction, message: str):
     substitutions = [('a', 'а'), ('e', 'е'), ('i', 'і'), ('p', 'р'),
@@ -215,30 +140,22 @@ async def scramble(interaction: discord.Interaction, message: str):
                      ('y', 'у')]
     for old, new in substitutions:
         message = message.replace(old, new)
-    message = '﻿'.join(list(message))
+    message = '​'.join(list(message))
     await interaction.response.send_message(message, ephemeral=True)
 
 @bot.tree.command(name="translate", description="translate messages to english")
 async def translate(interaction: discord.Interaction, message: str, lang: str = "en"):
     await interaction.response.send_message(translator(message, lang), ephemeral=True)
-        
-@bot.tree.command(name="channelinfo", description="get info from channel id")
-async def channelinfo(interaction: discord.Interaction, id: str):
-    await interaction.response.send_message(channel_name_from_id(id))
 
 @bot.tree.command(name="catgirl", description="send a catgirl")
 async def catgirl(interaction: discord.Interaction):
     await interaction.response.send_message(get_catgirl_link(), ephemeral=False)
 
-@bot.tree.command(name="diddy", description="generate a diddy")
-async def diddy(interaction: discord.Interaction):
-    await interaction.response.send_message("https://www.lafocusnews.com/wp-content/uploads/2023/08/Diddy-681x1024.jpg", ephemeral=False)
-
 def get_timezone_name(offset):
     gmt_offset_names = [
         (-12, "International Date Line West (IDLW)"),
         (-11, "Niue Time (NUT)"),
-        (-10, "Hawaii-Aleutian Time (HAT)"),
+        (-10, "Hawaii Time (HAT)"),
         (-9, "Alaska Time (AKT)"),
         (-8, "Pacific Time (PT)"),
         (-7, "Mountain Time (MT)"),
@@ -253,12 +170,12 @@ def get_timezone_name(offset):
         (2, "Eastern European Time (EET)"),
         (3, "Moscow Time (MSK)"),
         (4, "Gulf Standard Time (GST)"),
-        (5, "Pakistan Standard Time (PKT)"),
+        (5, "Kazakhstan Standard Time (PKT)"),
         (6, "Bangladesh Time (BST)"),
         (7, "Indochina Time (ICT)"),
         (8, "China Standard Time (CST)"),
         (9, "Japan Standard Time (JST)"),
-        (10, "Australian Eastern Time (AET)"),
+        (10, "Australian Time (AET)"),
         (11, "Solomon Islands Time (SBT)"),
         (12, "New Zealand Standard Time (NZST)")
     ]
@@ -267,29 +184,14 @@ def get_timezone_name(offset):
             return offset_name[1]
 
 @bot.tree.command(name="timezones", description="calculate timezones")
-async def timezones(interaction: discord.Interaction, their_time: int = None, your_time: int = None, offset: int = None):
-    now = datetime.now()
-    if your_time and their_time:
-        offset = (their_time - your_time) % 24
-        if offset > 12:
-            offset -= 24
-        offset_total = offset + user_gmt_offset
-        gmt_sign = "+" if offset_total > 0 else ""
-        mdt_sign = "+" if offset > 0 else ""
-        timezone_name = get_timezone_name(offset_total)
-        await interaction.response.send_message(
-            f"GMT{gmt_sign}{offset_total}, MDT{mdt_sign}{offset}, {timezone_name}",
-            ephemeral=True
-        )
-    elif their_time and offset:
-        your_time = (their_time - offset) % 24
-        await interaction.response.send_message(f"Your time is: {your_time}", ephemeral=True)
-    elif offset:
-        their_hour = (now.hour + offset - user_gmt_offset) % 24
-        await interaction.response.send_message(
-            f"Their time is: {their_hour}:{'0' if len(str(now.minute)) == 1 else ''}{now.minute}",
-            ephemeral=True
-        )
+async def timezones(interaction: discord.Interaction, gmt_offset: int = None):
+    if gmt_offset != None:
+        timezone_name = get_timezone_name(gmt_offset)
+        gmt_offset = timedelta(hours=gmt_offset)
+        utc_now = datetime.utcnow()
+        their_time = utc_now + gmt_offset
+        their_time = their_time.strftime("%I:%M %p")
+        await interaction.response.send_message(f"Their time is {their_time}, ({timezone_name})", ephemeral=True)
     else:
         await interaction.response.send_message("http://www.hoelaatishetnuprecies.nl/wp-content/uploads/2015/03/world-timezone-large.jpg")
 
@@ -328,19 +230,6 @@ async def downloader(interaction: discord.Interaction, num: int):
         file.writelines(reversed(lines))
     await interaction.followup.send(file=discord.File(filename), ephemeral=True)
 
-@bot.tree.command(name="bots", description="check active bots")
-async def bots(interaction: discord.Interaction, kill: int = 0):
-    if kill:
-        await interaction.response.send_message(execute_command(f"kill {kill}"), ephemeral=True)
-    else:
-        await interaction.response.send_message(execute_command("ps aux | grep python3 | head -n -2 | awk '{print $2, $12, $13, $14}'"), ephemeral=True)
-    
-@bot.tree.command(name="password", description="set password for encryption")
-async def password(interaction: discord.Interaction, password: str):
-    with open(os.path.expanduser(key_file), 'w') as f:
-        f.write(password)
-    await interaction.response.send_message("password set!", ephemeral=True)
-
 @bot.tree.command(name="date", description="make fancy date embed")
 async def date(interaction: discord.Interaction, month: int = None, day: int = None, hour: int = None, minute: int = None):
     now = datetime.now()
@@ -367,10 +256,7 @@ async def senakot_time(interaction):
 @bot.tree.command(name="hidetext", description="hide text behind other text")
 async def hidetext(interaction: discord.Interaction, showntext: str, hidetext: str):
     spoiler = "||​" * 400
-    await interaction.response.send_message(
-        f"```{showntext}  {spoiler} _ _ _ _ _ _  {hidetext}```",
-        ephemeral=True
-    )
+    await interaction.response.send_message(f"```{showntext}  {spoiler} _ _ _ _ _ _  {hidetext}```", ephemeral=True)
 
 @bot.tree.command(name="ai_tilley", description="make tilley an ai")
 async def ai_tilley(interaction: discord.Interaction):
