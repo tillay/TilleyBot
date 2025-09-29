c07057011371fa18795fa8f1a59e52da48a20d5b
Tilley <tilleyy8@gmail.com>
2025-09-29 13:05:42 -0600

    make ok for everyone to use

--- TilleyBot.py
+++ TilleyBot.py
@@ -4,15 +4,7 @@ from discord.ext import commands
 from html import unescape
 from openai import OpenAI
 
-try:
-    from Cryptodome.Util.Padding import pad, unpad
-    from Cryptodome.Cipher import AES
-except ModuleNotFoundError:
-    from Crypto.Util.Padding import pad, unpad
-    from Crypto.Cipher import AES
-
 bot_token_file = "~/bot_tokens/TilleyBot.token"
-user_token_file = "~/bot_tokens/tillay8.token"
 ai_token_file = "~/bot_tokens/deepseek.token"
 
 bot = commands.Bot("!", intents=discord.Intents.none())
@@ -20,44 +12,9 @@ bot = commands.Bot("!", intents=discord.Intents.none())
 def get_bot_token():
     with open(os.path.expanduser(bot_token_file), 'r') as f:
         return f.readline().strip()
-
-def get_user_token():
-    with open(os.path.expanduser(user_token_file), 'r') as f:
-        return f.readline().strip()
-
 def get_ai_token():
     with open(os.path.expanduser(ai_token_file), 'r') as f:
         return f.readline().strip()
-
-header_data = {
-    "Content-Type": "application/json",
-    "Authorization": get_user_token()
-}
-
-client = OpenAI(api_key=get_ai_token(), base_url="https://api.deepseek.com")
-
-def send_user_message(channel_id, message_content):
-    conn = http.client.HTTPSConnection("discord.com", 443)
-    message_data = json.dumps({
-        "content": message_content,
-        "tts": False
-    })
-    conn.request("POST", f"/api/v10/channels/{channel_id}/messages", message_data, header_data)
-    response = conn.getresponse()
-    if response.status == 200:
-        message_info = json.loads(response.read().decode())
-        return message_info['id']
-    else:
-        print(f"Failed to send message: {response.status} {response.reason}")
-        return None
-
-
-def send_and_delete(channel_id, message_content, delay = 0):
-    message_id = send_user_message(channel_id, message_content)
-    if delay: time.sleep(delay)
-    if message_id:
-        delete_url = f"https://discord.com/api/v10/channels/{channel_id}/messages/{message_id}"
-        requests.delete(delete_url, headers=header_data)
 
 def deepseek_query(prompt, context):
     client = OpenAI(api_key=get_ai_token(), base_url="https://api.deepseek.com")
@@ -70,16 +27,6 @@ def deepseek_query(prompt, context):
         stream=False
     )
     return response.choices[0].message.content
-
-def get_most_recent_message(channel_id):
-    conn = http.client.HTTPSConnection("discord.com", 443)
-    conn.request("GET", f"/api/v10/channels/{channel_id}/messages?limit=1", headers=header_data)
-    response = conn.getresponse()
-    if 199 < response.status < 300:
-        message = json.loads(response.read().decode())
-        return message[0]['content']
-    else:
-        return None
 
 def get_catgirl_link():
     url = "https://nekos.moe/api/v1/random/image"
@@ -110,21 +57,6 @@ def get_current_time(gmt_offset):
         time = time.strftime("%H:%M (%I:%M %p)")
         return time
 
-@bot.tree.command(name="repeat", description="repeat previous message")
-async def repeat(interaction: discord.Interaction):
-    channel_id = interaction.channel.id
-    await interaction.response.send_message("repeating", ephemeral=True)
-    send_user_message(channel_id, get_most_recent_message(interaction.channel.id))
-
-@bot.tree.command(name="pfp", description="Send pfp of a user")
-async def pfp(interaction: discord.Interaction, user: str):
-    try:
-        member = await bot.fetch_user(int(user))
-    except discord.NotFound:
-        await interaction.response.send_message("User not found.", ephemeral=True)
-        return
-    await interaction.response.send_message(member.display_avatar.url, ephemeral=True)
-
 @bot.tree.command(name="printas", description="Say something as the bot")
 async def printas(interaction: discord.Interaction, message: str):
     await interaction.response.send_message(message)
@@ -154,79 +86,27 @@ async def timezones(interaction: discord.Interaction, gmt_offset: int = None):
         utc_now = datetime.utcnow()
         their_time = utc_now + gmt_offset
         their_time = their_time.strftime("%I:%M %p")
-        await interaction.response.send_message(f"Their time is {their_time})", ephemeral=True)
+        await interaction.response.send_message(f"Their time is {their_time}", ephemeral=True)
     else:
         await interaction.response.send_message("http://www.hoelaatishetnuprecies.nl/wp-content/uploads/2015/03/world-timezone-large.jpg")
-
-@bot.tree.command(name="downloader", description="download messages")
-async def downloader(interaction: discord.Interaction, num: int):
-    filename = "messages.txt"
-    channel_id = interaction.channel.id
-    total_messages_to_fetch = num
-    messages_fetched = 0
-    before_id = None
-    await interaction.response.send_message(f"Downloading {num} messages from {interaction.channel.id}", ephemeral=True)
-    with open(filename, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        while messages_fetched < total_messages_to_fetch:
-            remaining_messages = total_messages_to_fetch - messages_fetched
-            current_batch_size = min(100, remaining_messages)
-            url = f"https://discord.com/api/v10/channels/{channel_id}/messages"
-            params = {"limit": current_batch_size}
-            if before_id:
-                params["before"] = before_id
-            response = requests.get(url, headers=header_data, params=params)
-            if response.status_code == 200:
-                messages = response.json()
-                if messages:
-                    for message in messages:
-                        writer.writerow([message['content']])
-                    messages_fetched += len(messages)
-                    before_id = messages[-1]["id"]
-                else:
-                    break
-            else:
-                break
-    with open(filename, mode='r', encoding='utf-8') as file:
-        lines = file.readlines()
-    with open(filename, mode='w', encoding='utf-8') as file:
-        file.writelines(reversed(lines))
-    await interaction.followup.send(file=discord.File(filename), ephemeral=True)
 
 @bot.tree.command(name="senakot_time", description="print info about senakot's time")
 async def senakot_time(interaction):
     await interaction.response.send_message(f"Senakot's current time is {get_current_time(4)}. He lives in UTC+4.")
 
-@bot.tree.command(name="kea_time", description="print info about doctorkea's time")
-async def senakot_time(interaction):
-    await interaction.response.send_message(f"Doctorkea's current time is {get_current_time(12)}. He lives in UTC+12.")
-    
-@bot.tree.command(name="neria_time", description="print info about neria's time")
-async def neria_time(interaction):
-    await interaction.response.send_message(f"Neria's current time is {get_current_time(3)}. He lives in UTC+3.")
+@bot.tree.command(name="kybe_time", description="print info about 2kybe3's time")
+async def kybe_time(interaction):
+    await interaction.response.send_message(f"Kybe's current time is {get_current_time(2)}. He lives in UTC+2.")
 
 @bot.tree.command(name="hidetext", description="hide text behind other text")
 async def hidetext(interaction: discord.Interaction, showntext: str, hidetext: str):
     spoiler = "||​" * 400
     await interaction.response.send_message(f"```{showntext}  {spoiler} _ _ _ _ _ _  {hidetext}```", ephemeral=True)
 
-@bot.tree.command(name="ai_tilley", description="make tilley an ai")
-async def ai_tilley(interaction: discord.Interaction):
-    context = "You are tilley8, a discord user. You like cats and Linux. Respond to this prompt very concisely, only 20 words or less. do not include emojis and be relatively serious"
-    prompt = get_most_recent_message(interaction.channel.id)
-    await interaction.response.send_message(f"generating message with prompt: {prompt}", ephemeral=True)
-    send_user_message(interaction.channel.id, deepseek_query(prompt, context))
-
 @bot.tree.command(name="deepseek", description="get response from deepseek")
 async def deepseek(interaction: discord.Interaction, prompt: str):
     await interaction.response.send_message(prompt)
     await interaction.followup.send(deepseek_query(prompt, "You are a discord bot called Tilley Bot who answers questions in a discord server. do not include emojis and be relatively serious. Keep all responses under 1950 characters."))
-
-@bot.tree.command(name="ghost_ping", description="send and delete message fast")
-async def ghost_ping(interaction: discord.Interaction, message: str, sniper: str=None, delay: int = None):
-    await interaction.response.send_message("you should feel guilty", ephemeral=True)
-    send_and_delete(interaction.channel.id, message, delay)
-    if sniper: send_and_delete(interaction.channel.id, sniper)
 
 @bot.tree.command(name="status_paid", description="send fake status paid message until noxbotv2 comes out")
 async def status_paid(interaction: discord.Interaction):
@@ -250,16 +130,6 @@ async def do_the_thing(interaction: discord.Interaction, message: str = None):
         await interaction.response.send_message(last_message)
     else:
         await interaction.response.send_message("please specify message", ephemeral=True)
-        
-@bot.tree.command(name="tldr", description="tldr a website")
-async def tldr(interaction: discord.Interaction, url: str):
-    r = requests.get(url, timeout=10)
-    if r.status_code != 200:
-        await interaction.response.send_message("Failed to download site", ephemeral=True)
-        return
-    html = r.text[:15000]
-    result = deepseek_query(html + "\n\nExplain this in simple words below 1600 characters", "")
-    await interaction.response.send_message(result)
     
 @bot.event
 async def on_ready():
